413f16911eee6ea87fa1d228149b89c6c9be7d10
amyou <amine.youssfi10@gmail.com>
2020-06-21 16:34:00 +0200

    Updates

---
+++ precache-manifest.c8d3165fbe64de554cbc67f1fbd942ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c8b611eac223bcf9ed2138e335a4b3",
+    "url": "/React-markdow-app/index.html"
+  },
+  {
+    "revision": "866c100c5e7ea286297f",
+    "url": "/React-markdow-app/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "35e5209959e8153f1352",
+    "url": "/React-markdow-app/static/css/main.4cef6e52.chunk.css"
+  },
+  {
+    "revision": "866c100c5e7ea286297f",
+    "url": "/React-markdow-app/static/js/2.42035f2f.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/React-markdow-app/static/js/2.42035f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e5209959e8153f1352",
+    "url": "/React-markdow-app/static/js/main.563e92e7.chunk.js"
+  },
+  {
+    "revision": "912429894fb79ceef971",
+    "url": "/React-markdow-app/static/js/runtime-main.ab7f6221.js"
+  }
+]);
